e877b9746ea5a6108a5d8da752ab1d5e693b1d92
Orlov Dmitry <orlovdimas92@mail.ru>
2021-05-11 08:50:42 +0300

    Reformat and rearrange code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int price = 125;
         int bonus;
-        if (price % 20 == 0) bonus=price/20; else bonus=(price-price%20)/20;
-            System.out.println(bonus);
+        if (price % 20 == 0) bonus = price / 20;
+        else bonus = (price - price % 20) / 20;
+        System.out.println(bonus);
     }
 }
